8a4e4d3a278ab50084f4f9296b74e67ad6205337
Jacek Artymiak <jacek@artymiak.com>
2013-08-18 22:30:47 +0100

    Added TornadoWeb example.

---
+++ tornadoweb.py
@@ -0,0 +1,23 @@
+import tornado.ioloop
+import tornado.web
+import calculator
+
+class CalculateHandler(tornado.web.RequestHandler):
+    def post(self):
+        c = calculator.simple.SimpleCalculator()
+
+        try:
+            c.run(self.request.body)
+            for i in c.h:
+                self.write("%s\n" % str(i).encode('utf-8'))
+
+        except Exception as e:
+            self.send_error()
+
+application = tornado.web.Application([
+    (r"/v1/calculate", CalculateHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
