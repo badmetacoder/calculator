75141ada6aa283cc5f8390602dc7fdff692a3493
Jacek Artymiak <jacek@artymiak.com>
2013-08-20 11:54:15 +0000

    Building a PyPI package.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='calculator',
       author_email='jacek@artymiak.com',
       url='https://github.com/badmetacoder/calculator',
       version='0.0.1',
-      py_modules=['calculator', 'calculator.simple'],
+      py_modules=['calculator.simple'],
       )
