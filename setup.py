2e9c68a75098e04034703e967a836a1233215695
Ubuntu <ubuntu@ip-10-48-47-129.eu-west-1.compute.internal>
2013-08-20 11:46:32 +0000

    Repo reorg.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='calculator',
+      description='A simple calculator library.',
+      author='Jacek Artymiak',
+      author_email='jacek@artymiak.com',
+      url='https://github.com/badmetacoder/calculator',
+      version='0.0.1',
+      py_modules=['calculator', 'calculator.simple'],
+      )
